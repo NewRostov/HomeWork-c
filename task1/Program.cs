f1f3ff14278ebe369e41196337d7c664588cbed7
NewRostov <kim.sergey.rostov@gmail.com>
2023-03-12 21:05:44 +0100

    init commit

--- task1/Program.cs
+++ task1/Program.cs
@@ -8,6 +8,6 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число b: ");
 int b = Convert.ToInt32(Console.ReadLine());
 if (a > b) 
-{Console.WriteLine( "Максимальное число a = " + a + "; " + "a > b; " + "a = " + a + "; " + "b = " + b  + ";" );}
+{Console.WriteLine( " Максимальное число a = " + a + "; " + "a > b; " + "a = " + a + "; " + "b = " + b  + ";" );}
 else 
-{Console.WriteLine( "Максимальное число b = " + b + "; " + "a < b; " + "a = " + a + "; " + "b = " + b  + ";" );}
+{Console.WriteLine( " Максимальное число b = " + b + "; " + "a < b; " + "a = " + a + "; " + "b = " + b  + ";" );}
