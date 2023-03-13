4583b77f541e4e248e1b2f547a64355d0c064001
NewRostov <kim.sergey.rostov@gmail.com>
2023-03-13 14:18:46 +0100

    задача 7

---
+++ task5/Program.cs
@@ -0,0 +1,23 @@
+﻿//  Напишите программу, которая принимает на вход целое число любой разрядности и 
+//на выходе показывает вторую цифру слева этого числа или говорит, что такой цифры нет.
+//Через строку решать нельзя.
+//456111 -> 5
+//78 -> 8
+//9146548 -> 1
+//3 -> нет
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 10) { Console.WriteLine("Остатка нет"); } 
+else if (number < 100) { Console.WriteLine(number % 10); }
+else
+{
+do
+{
+ number = number/10;
+}
+while (number > 100);
+Console.WriteLine(number % 10);;
+}
+
+
